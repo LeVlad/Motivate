484873f0971866a6f645dc89eb94cbe6fd11c26a
LeVlad <leca.vlad@gmail.com>
2021-12-10 19:55:39 +0000

    Add new function to get random quotes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContedLoaded", function(){
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons){
-       button.addEventListener("click", function(){
+       buttons.addEventListener("click", function(){
            if(this.getAttribute("data-type") === "submit"){
                alert("you clicked submit!");
            } else {
@@ -10,3 +10,16 @@ document.addEventListener("DOMContedLoaded", function(){
            }
        })
 })
+
+function getRandomQuote(){
+    let quotes = ["Be positive. Be true. Be kind.Roy T. Bennett",
+    "You cannot have a positive life and a negative mind.Joyce Meyer",
+    "If you are positive, you’ll see opportunities instead of obstacles.Widad Akrawi",
+    "Write it on your heart that every day is the best day in the year.Ralph Waldo Emerson",
+    "Accentuate the positive, Eliminate the Negative, latch onto the affirmative.Bing Crosby",
+    "Positive anything is better than negative nothing.Elbert Hubbard",
+    "Miracles happen to those who believe in them.Bernhard Berenson",
+    "One small positive thought can change your whole day.Zig Ziglar"];
+   }
+   
+}
